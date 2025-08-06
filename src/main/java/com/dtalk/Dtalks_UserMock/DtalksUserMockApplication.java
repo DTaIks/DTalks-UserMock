89f59b832ad4653e373966cd686b020f567f125c
hdsjiw <bisha0821@gmail.com>
2025-08-06 18:22:11 +0900

    init: Spring Boot UserMock 프로젝트 초기 세팅

---
+++ src/main/java/com/dtalk/Dtalks_UserMock/DtalksUserMockApplication.java
@@ -0,0 +1,13 @@
+package com.dtalk.Dtalks_UserMock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtalksUserMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtalksUserMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtalk/Dtalks_UserMock/DtalksUserMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtalk.Dtalks_UserMock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtalksUserMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
